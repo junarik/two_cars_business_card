be927391777a3e9f2842e7db271c5fa15cba39b5
junarik <113181044+junarik@users.noreply.github.com>
2024-11-02 14:24:04 +0200

    added-angular-animation-in-purchased-cars-component

--- firstwork/src/app/main/main.component.ts
+++ firstwork/src/app/main/main.component.ts
@@ -24,7 +24,7 @@ export class MainComponent implements OnInit {
   ];
 
   displayedItems: string[] = [];
-  private itemsPerClick: number = 6;
+  private itemsPerClick: number = 3;
 
   ngOnInit(){
     this.loadMoreItems();
--- firstwork/src/app/main/purchased-cars-grid/purchased-cars-grid.component.ts
+++ firstwork/src/app/main/purchased-cars-grid/purchased-cars-grid.component.ts
@@ -1,16 +1,35 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-purchased-cars-grid',
   templateUrl: './purchased-cars-grid.component.html',
   styleUrl: './purchased-cars-grid.component.css',
-  // animations: [
-  //   trigger('animation', [
-  //     transition('* <=> *', [
-  //       query(':enter', [ style({ opacity: 0, transform: 'scale(0,7)' })]),
-  //       animate('500ms ease-in', style({opacity: 1, transform: 'scale(1)'}))
-  //     ]),
-  //   ]),
-  // ],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(50px)' }),
+            stagger(100, [
+              animate(
+                '500ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class PurchasedCarsGridComponent {
   @Input() showLoadMore!: boolean;
