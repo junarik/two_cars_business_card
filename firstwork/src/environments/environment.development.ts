f5e2de0cb8edafd6b99d5c7f4ab20227ba408d3c
dim4ik9228 <serhii.kukharchuk@oa.edu.ua>
2024-11-04 23:49:37 +0200

    adjusts baseApiUrl env variable

--- firstwork/src/environments/environment.development.ts
+++ firstwork/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     prodution: true,
-    baseApiUrl: 'https://twocars.salesdrive.me/handler/',
+    baseApiUrl: 'https://twocars.salesdrive.me',
     crmApikey: 'jRISHvQrO1ivuUJlqHPrarcQnnoyKdqyTHSGk8b7fCiy4VpLqFvbgba9jNjtVqjvMCgw'
 };
--- firstwork/src/environments/environment.ts
+++ firstwork/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseApiUrl: 'https://twocars.salesdrive.me/handler/',
+    baseApiUrl: 'https://twocars.salesdrive.me',
     crmApikey: 'jRISHvQrO1ivuUJlqHPrarcQnnoyKdqyTHSGk8b7fCiy4VpLqFvbgba9jNjtVqjvMCgw'
 };
